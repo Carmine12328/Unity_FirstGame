c2312fa444cc1da0320a099aa65c6c191a3cdc8a
Carmine12328 <carmine12328@gmail.com>
2020-01-06 16:14:13 +0100

    Added movement class and check how movement works

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //se premo il tasto muovi l'oggetto sull'asse corrispondente al tasto
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            if(Input.GetAxis("Horizontal") > 0)
+            {
+                Debug.Log("Stai andando a destra");
+            }
+            else
+            {
+                Debug.Log("Stai andando a sinistra");
+            }
+        }
+    }
+}
